bf31a8ebc84f2e391454d196f2b63882a19fb1d7
Joshua Hutchinson <jthutch94@gmail.com>
2018-02-21 14:51:05 -0500

    object-drills1

--- object-drills1.js
+++ object-drills1.js
@@ -12,12 +12,17 @@ for (let key in obj) {
 const meals= ['breakfast','second breakfast','elvenses','lunch','afternoon tea','dinner','supper'];
 console.log(meals[5]);
 
-let obj1 = {name:'Jacob',jobTitle:'CSR'};
-let obj2 = {name:'Bob',jobTitle:'Manager'};
-let obj3 = {name:'Spongebob',jobTitle:'Frycook'};
+let obj1 = {name:'Jacob',jobTitle:'CSR',boss:'reports to Bob'};
+let obj2 = {name:'Bob',jobTitle:'Manager', boss:'doesnt report to anyone'};
+let obj3 = {name:'Spongebob',jobTitle:'Frycook',boss:'reports to Bob'};
 
 let arr1 = [obj1, obj2, obj3];
 
 for (let i = 0; i < arr1.length; i++) {
     console.log(arr1[i].name +": "+ arr1[i].jobTitle);
 }
+
+for(let i=0;i<arr1.length;i++){
+    console.log(`${arr1[i].jobTitle} ${arr1[i].name} ${arr1[i].boss}`);
+}
+
