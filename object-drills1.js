1fcc8919dd3ada86eb063ce30aef5ff2de94f29a
JacobAZimmerman <ijacobzim@gmail.com>
2018-02-21 12:18:42 -0800

    recreated decode with an object

--- object-drills1.js
+++ object-drills1.js
@@ -26,3 +26,28 @@ for(let i=0;i<arr1.length;i++){
     console.log(`${arr1[i].jobTitle} ${arr1[i].name} ${arr1[i].boss}`);
 }
 
+function decode(word) {
+    const obj = {a:2,b:3,c:4,d:5};
+    if (!Object.keys(obj).find(key => key === word[0])) {
+        return " ";
+    } if (word.charAt(0) === Object.keys(obj)[0]) {
+        return word.charAt(1);
+    } if (word.charAt(0) === Object.keys(obj)[1]) {
+        return word.charAt(2);
+    } if (word.charAt(0) === Object.keys(obj)[2]) {
+        return word.charAt(3);
+    } if (word.charAt(0) === Object.keys(obj)[3]) {
+        return word.charAt(4);
+    }
+}
+
+function decodeWords(words) {
+    const array = words.split(" ");
+    let word = '';
+    for (let i = 0; i < array.length; i++) {
+        word += decode(array[i]);
+    }
+    return word;
+}
+
+console.log(decodeWords('craft block argon meter bells brown croon droop'));
